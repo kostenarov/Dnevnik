abdcbc2723d5031c13d7e0ec6fea0816b17f18fd
kostenarov <61360809+kostenarov@users.noreply.github.com>
2022-06-19 22:58:43 +0300

    Update Console_interface.cpp

--- Interface/Console_interface.cpp
+++ Interface/Console_interface.cpp
@@ -2,58 +2,117 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+
 using namespace std;
 
-class User
+class Story
 {
     private:
-        string username;
-        string password;
+        string date;
+        string storyName;
+        string story;
     
     public:
-        User(const string& username, const string& password)
+        Story(const string& date, const string& storyName, const string& story)
         {
-            if(username.empty() && password.empty())
+            if(date.empty() && storyName.empty() && story.empty())
                 throw "Invalid data";
-            this->username = username;
-            this->password = password;
+            this->date = date;
+            this->storyName = storyName;
+            this->story = story;
         }
-        const string& getName()const{  return this->username;  }
-        const string& getPassword()const{  return this->password;  }
+        const string& getDate()const{  return this->date;  }
+        const string& getStoryName()const{  return this->storyName;  }
+        const string& getStory()const{  return this->story;  }
+        
+        void writeStory(const Story& story1, const string& user)
+        {
+            fstream newfile;
+            string fileName;
+            fileName = user + ".txt";
+            newfile.open(fileName,ios::app);  
+            if(newfile.is_open()) 
+            {
+                newfile << "Story date: " << story1.getDate() << endl << "Story name: " << story1.getStoryName() << endl << "Story: " << endl << story1.getStory() << endl;  
+                newfile.close();    
+            }
+        }
 };
 
+void menu(const string& username)
+{
+    size_t choice;
+    while(choice != 3)
+    {
+        cout << "1: Write a story." << endl << "2: Read your stories." << endl << "3:Exit." << endl;
+        cin >> choice;
+        switch(choice)
+        {
+            case 1:
+            {
+                string date, storyName, storyTemp, story;
+                cout << "Date:";
+                cin >> date;
+                cout << "Story:";
+                cin >> storyName;
+                cout << "Enter story:";
+                while(getline(cin, storyTemp))
+                {
+                    story += storyTemp;
+                }
+                Story story1(date, storyName, story);
+                story1.writeStory(story1, username);
+            }
+            case 2:
+                break;
+        }
+    }
+}
+
+void login()
+{
+    cout << "1: Login" << endl << "2: Register";
+    size_t choice;
+    cin >> choice;
+    fstream newfile;
+    switch(choice)
+    {
+        case 1:
+        {
+            string username, password, data;
+            cin >> username;
+            cin >> password;
+            if(newfile.is_open()) 
+            {
+                while(getline(newfile, data)) 
+                {
+                    if(data == username + " " + password)
+                        menu(username);
+                }
+            }
+        }
+            
+        case 2:
+        {
+            string username, password, data;
+            cin >> username;
+            cin >> password;
+            newfile.open("logins.txt",ios::in, ios::app);  
+            if(newfile.is_open()) 
+            {
+                while(getline(newfile, data)) 
+                {
+                    if(data == username + " " + password)
+                        newfile << username << " " << password << endl;
+                        menu(username);
+                }
+            }
+        }
+    }
+}
 
 int main()
 {
-    fstream newfile;
-    string username, password;
     
-    cout << "Please input valid username and password" << endl;
-    cout << "Username:";
-    cin >> username;
-    cout << "Password:";
-    cin >> password;
-    User user1(username, password);
-    newfile.open("logins.txt",ios::app);  
-    if(newfile.is_open()) 
-    {
-        newfile<<user1.getName() << " " << user1.getPassword() << endl;   
-        newfile.close();    
-    }
-    newfile.open("logins.txt",ios::in); 
-    if (newfile.is_open())
-    {   
-          string tp;
-          while(getline(newfile, tp))
-          { 
-              if(username + " " + password == tp)
-              {
-                  string filename = username + ".txt";
-                  newfile.open(filename, ios::app);
-                  newfile << "dab";
-              }
-          }
-          newfile.close(); 
-    }
     return 0;
 }
