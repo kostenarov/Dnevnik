db752a36b01d4ab9ff7e33431ad8144703766518
Boris-Elsys <71979318+Boris-Elsys@users.noreply.github.com>
2022-06-19 22:34:33 +0300

    Update users.cpp

--- Users/users.cpp
+++ Users/users.cpp
@@ -6,18 +6,36 @@ using namespace std;
 
 class User
 {
-    private:
-        string username;
-        string password;
+    string username;
+    string password;
+    vector <Diary> diary;
+    //if multiple diaries are needed
     
     public:
-        User(const string& username, const string& password)
-        {
-            if(username.empty() && password.empty())
-                throw "Invalid data";
+        User(const string& username, const string& password){
+            
+        if(username.empty() || password.empty())
+            throw invalid_argument("Invalid data");
             this->username = username;
             this->password = password;
         }
+        
         const string& getName()const{  return this->username;  }
         const string& getPassword()const{  return this->password;  }
+        
+        void setName(string username){  
+            if(!username.empty())
+                throw invalid_argument("Invalid data");
+            this->username = username;
+        }
+        void setPassword(string password){        
+            if(!password.empty())
+                throw invalid_argument("Invalid data");
+            this->password = password;
+        }
 };
+
+int main(){
+
+    return 0;
+}
