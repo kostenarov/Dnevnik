9fa3bdd3c80bd8ccb371ef523d3d169e709e70fa
Boris-Elsys <71979318+Boris-Elsys@users.noreply.github.com>
2022-06-19 22:51:12 +0300

    Update users.cpp

--- Users/users.cpp
+++ Users/users.cpp
@@ -8,36 +8,48 @@ class User
 {
     string username;
     string password;
-    vector <Diary> diary;
+    vector<Diary> diaries;
     //if multiple diaries are needed
     
     public:
-        User(const string& username, const string& password){
-            
-        if(username.empty() || password.empty())
-            throw invalid_argument("Invalid data");
-            this->username = username;
-            this->password = password;
+        User(const string& username, const string& password, vector<Diary> diaries)
+        : username(username), password(password), diaries(diaries)
+        {
+            if(username.empty() || password.empty() || diaries.empty())
+                throw invalid_argument("Invalid data");
         }
         
         const string& getUsername()const{  return this->username;  }
         const string& getPassword()const{  return this->password;  }
+        const vector<Diary>& getDiaries() const { return this->diaries; }
         
-        void setUsername(const string& username){  
+        void setUsername(const string& username){ 
+            
             if(!username.empty())
                 throw invalid_argument("Invalid data");
+                
             this->username = username;
         }
-        void setPassword(const string& password){        
+        void setPassword(const string& password){  
+            
             if(!password.empty())
                 throw invalid_argument("Invalid data");
+                
             this->password = password;
         }
+        
+        User& addDiary(const Diary& diary){
+            
+            if(diary.empty())
+                throw invalid_argument("Invalid diary provided!");
+                
+            this->diaries.push_back(diary);
+            return *this;
+        }
+
 };
 
 int main(){
     
-    printf("Hello World");
-
     return 0;
 }
