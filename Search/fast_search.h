d9ee812361bc7758cbb914ed3ce440c72bff3024
Nikola Kerezov <72574892+NikiKerezov@users.noreply.github.com>
2022-06-26 22:15:06 +0300

    Update fast_search.h

--- Search/fast_search.h
+++ Search/fast_search.h
@@ -21,38 +21,6 @@ void addElementToNamesTable(string key, Story& story, unordered_map<string, Stor
     map[key] = story;
 }
 
-void removeElementFromDatesTable(string key, Story& story, unordered_map<string, vector<Story>>& map)
-{
-    if(key.empty())
-        throw invalid_argument("No key provided!");
-
-    if(map[key].empty())
-        throw invalid_argument("There is no such story!");
-
-    if(map[key].size() > 1)
-    {
-        cout << "There are multiple stories here!" << endl;
-        for(int i = 0; i < map[key].size(); ++i)
-            cout << map[key][i].getStoryName() << endl;
-        cout << "Choose which one you want! (1, 2, 3... etc)" << endl;
-
-        short index;
-        cin >> index;
-        //map[key].erase(map[key].begin() + index - 1);
-        return;
-    }
-
-    map[key].pop_back();
-}
-
-void removeElementFromNamesTable(string key, Story& story, unordered_map<string, Story>& map)
-{
-    if(key.empty())
-        throw invalid_argument("No key provided!");
-
-    map.erase(key);
-}
-
 Story& fastSearchDate(string key, unordered_map<string, vector<Story>> map)
 {
     if(key.empty())
