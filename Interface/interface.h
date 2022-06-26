6bf7230c4f2d988691407df13fe038454b972927
Boris-Elsys <71979318+Boris-Elsys@users.noreply.github.com>
2022-06-26 19:57:56 +0300

    Update interface.h

--- Interface/interface.h
+++ Interface/interface.h
@@ -5,6 +5,104 @@
 #include "users.h"
 #include "search.h"
 
+class Story
+{
+    private:
+        string date;
+        string storyName;
+        string story;
+    
+    public:
+        Story() {}
+        
+        Story(const string& date, const string& storyName, const string& story)
+        {
+            if(date.empty() && storyName.empty() && story.empty())
+                throw "Invalid data";
+            this->date = date;
+            this->storyName = storyName;
+            this->story = story;
+        }
+        
+        Story& operator=(Story& other) {
+            if(this == &other) {
+                return *this;
+            }
+            
+            this->date = other.date;
+            this->storyName = other.storyName;
+            this->story = other.story;
+            
+            return *this;
+        }
+        
+        const string& getDate()const{  return this->date;  }
+        const string& getStoryName()const{  return this->storyName;  }
+        const string& getStory()const{  return this->story;  }
+        void setStory(const string& story)
+        {
+            this->story = story;
+        }
+        
+        void writeStory(const Story& story1, const string user, string password, string& vect)
+        {
+            fstream newfile;
+            string fileName, storyTemp, storyName, date;
+            storyTemp = story1.getStory();
+            storyName = story1.getStoryName();
+            date = story1.getDate();
+            
+            fileName = user + ".txt";
+            newfile.open(fileName, ios::app);
+            
+            if(newfile.is_open()) 
+            {
+                
+                CBC(date, password, vect);
+                CBC(storyName, password, vect);
+                CBC(storyTemp, password, vect);
+                
+                storyTemp += '#';
+                newfile << date << endl << storyName << endl << storyTemp << endl;  
+                newfile.close();    
+            }
+        }
+        
+        void readStory(fstream& newfile, string password, string& vect) 
+        {
+            string storyTemp, date, name, input;
+        
+            getline(newfile, date);
+            DeCBC(date, password, vect);
+                        
+            getline(newfile, name);
+            
+            DeCBC(name, password, vect);
+            
+            getline(newfile, storyTemp, '#');
+                        
+            DeCBC(storyTemp, password, vect);
+                
+            cout << storyTemp << endl << date << endl << name << endl;
+                        
+            this->date = date;
+            this->storyName = name;
+            this->story = storyTemp;
+        }
+        
+        friend ostream& operator<<(ostream& out, Story& other)
+        {
+            out << other.date << endl;
+            out << other.storyName << endl;
+            out << other.story << endl;
+            
+            return out;
+        }
+        
+        //friend fstream& operator>>(fstream)
+};
+
+
 void search()
 {
     /*
@@ -95,76 +193,6 @@ void menu(User& user)
                 search();
                 break;
         }
-    }
-}
-
-void login()
-{
-    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    cout << "1: Login" << endl << "2: Register"<< endl;
-    size_t choice;
-    cin >> choice;
-    switch(choice)
-    {
-        case 1:
-        {
-            fstream newfile;
-            string username, password, data;
-            
-            cout << "Enter username: ";
-            cin >> username;
-            cout << "Enter password: ";
-            cin >> password;
-
-            newfile.open("logins.txt", ios::in);
-            
-            if(newfile.is_open()) 
-            {
-                while(getline(newfile, data)) 
-                {
-                    hash<string> hash_obj;
-                    string hashedPass;
-                
-                    for(int i = 0; i < 53; i++){
-                        hashedPass = to_string(hash_obj(password + alphabet[i]));
-        
-                        if(data == username + "\t" + hashedPass){
-                            User user(username, hashedPass);
-                            menu(user);
-                            break;
-                        }
-                    }
-                }
-            }
-            
-            newfile.close();
-            break;
-        };
-            
-        case 2:
-        {
-            fstream newfile;
-            string username, password, data;
-            
-            cout << "Enter username: ";
-            cin >> username;
-            cout << "Enter password: ";
-            cin >> password;
-            
-            hash<string> hash_obj;
-            string hashedPass = to_string(hash_obj(password + alphabet[rand() % 52]));
-            
-            User user(username, hashedPass);
-            
-            newfile.open("logins.txt",ios::app);  
-            if(newfile.is_open()) 
-            {
-                newfile << user.getUsername() << "\t" << user.getPassword() << endl;
-                newfile.close();
-                menu(user);
-            }
-            break;
-        };
     }
 }
 
